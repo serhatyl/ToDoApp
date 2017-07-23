b3875e928a1cdf09b589e7216d4438b7eb21116a
Serhat Yılmaz <yilmaz.srhat@gmail.com>
2017-07-23 22:41:24 +0300

    Add Task Eklendi
    Task ekleme işlemi eklendi

--- components/main.js
+++ components/main.js
@@ -4,15 +4,24 @@ import {AddNewTask} from './addtask';
 import {ToDoAppList} from './applist';
 
 export class Todo extends React.Component{
-  constructor(){
+  constructor(props){
     super();
+    this.state = {tasks:props.tasks};
+    this.updateList=this.updateList.bind(this);
   }
+
+  updateList(text){
+    var updatedTasks = this.state.tasks;
+    updatedTasks.push(text);
+    this.setState({tasks:updatedTasks});
+  }
+
   render(){
     return (
       <div>
         <h1>ToDo App</h1>
-        <AddNewTask/>
-        <ToDoAppList tasks={this.props.tasks}/>
+        <AddNewTask updateList={this.updateList} />
+        <ToDoAppList tasks={this.state.tasks}/>
       </div>
     );
   }
